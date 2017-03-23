781681a2d3acb284e8a9f7b5a2d027a67a5e175d
SonyaLing <sonya.ling@rovicorp.com>
2017-03-23 12:20:36 -0700

    danube mongo 3/23

--- src/main/scala/org/freemind/spark/sql/DanubeMongoAnalysis.scala
+++ src/main/scala/org/freemind/spark/sql/DanubeMongoAnalysis.scala
@@ -39,6 +39,11 @@ object DanubeMongoAnalysis {
     val fanProdDS = spark.read.textFile(fanProdPath).map(parseMongoStats).withColumnRenamed("count", "count_fantv_prod").cache()
     val fanDevDS = spark.read.textFile(fanDevPath).map(parseMongoStats).withColumnRenamed("count", "count_fantv_dev").cache()
 
+    val countProd = fanProdDS.filter(!$"resource".startsWith("MX") && !$"resource".startsWith("Published")).groupBy().sum("count_fantv_prod").first().getLong(0)
+    val countDev = fanDevDS.filter(!$"resource".startsWith("MX") && !$"resource".startsWith("Published")).groupBy().sum("count_fantv_dev").first().getLong(0)
+
+    println(s"Total Jenga resource count in PROD vs DEV and (DEV - PROD): ${countProd}, ${countDev}, ${countDev - countProd}")
+
     val joinedDS = fanProdDS.join(fanDevDS, Seq("resource"), "right_outer")
               .withColumn("diff", when(isnull($"count_fantv_prod"), $"count_fantv_dev").otherwise($"count_fantv_dev" - $"count_fantv_prod") )
               .withColumn("difference", format_string("%,+8d", $"diff")).sort("resource")
